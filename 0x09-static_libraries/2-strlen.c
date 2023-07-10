4c22626c7d4c4c6b4e9c1603286c0a8f855f3fdf
programSha <sharonkirwa19@gmail.com>
2023-07-10 06:57:52 -0700

    update all these!

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,3 +1,20 @@
 #include "main.h"
+
+/**
+ * _strlen - returns lenght of string
+ * @s: input parameter
+ * Description: uses pointers
+ * Return: Alawy (0)
+ */
+
 int _strlen(char *s)
-{}
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
